f1fd12f7142f37b5945d7369aad4dbb2bbfe7a7c
brandon <bsbrinoesos@gmail.com>
2020-07-28 16:13:19 -0500

    'search/form submit functional'

--- client/clientCode/pokeApp.js
+++ client/clientCode/pokeApp.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import PokeView from './pokeView'
 
+
 class PokeApp extends Component {
 constructor(props) {
   super(props); 
   this.state = {
-    currentPokemon: {}
+    currentPokemon: {},
+    name: ''
   }
-
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
 }
 
 ///// during first render of pokeApp i would want a pokemon already up///
 componentDidMount() {
   this.firstSearch()
-  // .then(pokeData => {
-  //   this.setState({
-  //     currentPokemon: pokeData
-  //   })
-  // })
 }
-
+//// creating the first search function
 firstSearch() {
   const {getPokemon} = this.props
   //// call on a service that take in a number or name of pokemon//////
@@ -31,6 +29,25 @@ firstSearch() {
   })
 })
 }
+///// creating a search function////
+handleChange(event) {
+  this.setState({
+    name: event.target.value
+  })
+}
+
+handleSubmit(event) {
+  event.preventDefault()
+  const {getPokemon} = this.props
+
+  return getPokemon(this.state.name)
+ .then(pokeData => {
+  this.setState({
+    currentPokemon: pokeData
+  })
+})
+
+}
 
 
   render() {
@@ -39,6 +56,15 @@ firstSearch() {
     return (
       <div>
         <h1>Pokemon, Team builder!</h1>
+          <div className='form'>
+            <form onSubmit={this.handleSubmit}> 
+              <label>
+                PokeName:
+                <input type='text' value={this.state.name} onChange={this.handleChange}/>
+              </label>
+              <input type='submit' value="Submit" />
+            </form>
+          </div>
           <PokeView pokemon={this.state.currentPokemon}/>
       </div>
     ); 
