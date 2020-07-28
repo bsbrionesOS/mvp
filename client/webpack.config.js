6ec689e73943ea071d05f3cac38bd56339018c6e
brandon <bsbrinoesos@gmail.com>
2020-07-28 01:18:37 -0500

    'have react rendering'

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './clientCode/app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
